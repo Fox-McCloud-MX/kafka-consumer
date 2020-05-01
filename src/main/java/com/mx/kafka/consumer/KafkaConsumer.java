cad861ccb77829aeb7c46d9db8e7aa17399c0138
framirez90 <framirez.work@gmail.com>
2020-04-30 21:00:21 -0500

    Kafka Consumer refactor

--- src/main/java/com/mx/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/mx/kafka/consumer/KafkaConsumer.java
@@ -1,10 +1,7 @@
 package com.mx.kafka.consumer;
 
-import com.mx.kafka.config.KafkaConsumerConfig;
-import com.mx.kafka.config.KafkaProducerConfig;
 import com.mx.kafka.enity.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
